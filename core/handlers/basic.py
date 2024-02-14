1a8c36d0d252d0ff2d23c33fd2510b6e21bd09d8
KonstBeliakov <konstbeliakov@gmail.com>
2024-02-14 17:45:01 +0300

    Хандлеры теперь корректно обрабатывают сообщение не соответствующее тексту кнопок

--- core/handlers/basic.py
+++ core/handlers/basic.py
@@ -32,7 +32,7 @@ async def get_started(message: Message, bot: Bot, state: FSMContext):
 
 async def keyboard_sellect_lesson(message: Message, bot: Bot, state: FSMContext):
     if user_keyboard(message.from_user.username):
-        await bot.send_message(message.from_user.id, f'Выбери урок к которому хочешь оставить отзыв:',
+        await bot.send_message(message.from_user.id, f'Выбери урок к которому хочешь оставить отзыв',
                                reply_markup=user_keyboard(message.from_user.username))
         await state.set_state(BotStates.LESSON_SELECTION)
     else:
@@ -49,31 +49,49 @@ async def admin_start_keyboard(message: Message, bot: Bot, state: FSMContext):
             await keyboard_sellect_lesson(message, bot, state)
         case 'Обновить данные бота':
             await update_bot(message, bot, state)
+        case _:
+            await bot.send_message(message.from_user.id, f'Не получилось найти команду "{message.text}".'
+                                                         f' Пожалуйста выбери одну из доступных комманд',
+                                   reply_markup=admin_keyboard)
+            await state.set_state(BotStates.ADMIN_START)
 
 
 async def select_lesson(message: Message, bot: Bot, state: FSMContext):
     global selected_lesson
-    selected_lesson[message.from_user.username] = message.text
-    await bot.send_message(message.from_user.id, f'Теперь выбери тип отзыва для урока {message.text}',
-                           reply_markup=review_type_select_keyboard)
-
-    await state.set_state(BotStates.REVIEW_TYPE_SELECTION)
+    print([(button[0], type(button[0])) for button in user_keyboard(message.from_user.username).keyboard])
+    if message.text in [button[0].text for button in user_keyboard(message.from_user.username).keyboard]:
+        selected_lesson[message.from_user.username] = message.text
+        await bot.send_message(message.from_user.id, f'Теперь выбери тип отзыва для урока {message.text}',
+                               reply_markup=review_type_select_keyboard)
+        await state.set_state(BotStates.REVIEW_TYPE_SELECTION)
+    else:
+        await bot.send_message(message.from_user.id,
+                               f'Урок "{message.text}" не проводится в твоем классе. Пожалуйста выбери один из доступных',
+                               reply_markup=user_keyboard(message.from_user.username))
+        await state.set_state(BotStates.LESSON_SELECTION)
 
 
 async def select_review_type(message: Message, bot: Bot, state: FSMContext):
     global selected_review_type
-    selected_review_type[message.from_user.username] = message.text
-    await bot.send_message(message.from_user.id,
-                           f'Теперь можешь написать {"анонимный " if message.text == "Анонимный отзыв" else ""}отзыв '
-                           f'к уроку {selected_lesson[message.from_user.username]}')
-    await state.set_state(BotStates.WAITING_REVIEW)
+    if message.text in ['Анонимный отзыв', 'Обычный отзыв']:
+        selected_review_type[message.from_user.username] = message.text
+        await bot.send_message(message.from_user.id,
+                               f'Теперь можешь написать {"анонимный " if message.text == "Анонимный отзыв" else ""}отзыв '
+                               f'к уроку {selected_lesson[message.from_user.username]}')
+        await state.set_state(BotStates.WAITING_REVIEW)
+    else:
+        await bot.send_message(message.from_user.id, f'Не получилось найти тип отзыва "{message.text}". '
+                                                     f'Пожалуйста выбери один из доступных типов',
+                               reply_markup=review_type_select_keyboard)
+        await state.set_state(BotStates.REVIEW_TYPE_SELECTION)
 
 
 async def wait_review(message: Message, bot: Bot, state: FSMContext):
     await bot.send_message(message.from_user.id, f'Сохраняем отзыв...')
     try:
         write_review(username=message.from_user.username, lesson=selected_lesson[message.from_user.username],
-                     anonimous=selected_review_type[message.from_user.username] == 'Анонимный отзыв', review_text=message.text)
+                     anonimous=selected_review_type[message.from_user.username] == 'Анонимный отзыв',
+                     review_text=message.text)
     except Exception as err:
         await bot.send_message(message.from_user.id, f'При сохранении отзыва произошла ошибка: {err}')
     else:
@@ -82,9 +100,14 @@ async def wait_review(message: Message, bot: Bot, state: FSMContext):
 
 
 async def try_again(message: Message, bot: Bot, state: FSMContext):
-    if message.text == 'Попробовать еще раз':
-        await update_bot(message, bot, state)
-    else:
-        await bot.send_message(message.from_user.id, f'Ты в списке администраторов! Тебе доступна возможность '
-                                                     f'обновления данных бота', reply_markup=admin_keyboard)
-        await state.set_state(BotStates.ADMIN_START)
+    match (message.text):
+        case 'Попробовать еще раз':
+            await update_bot(message, bot, state)
+        case 'Назад':
+            await bot.send_message(message.from_user.id, f'Ты в списке администраторов! Тебе доступна возможность '
+                                                         f'обновления данных бота', reply_markup=admin_keyboard)
+            await state.set_state(BotStates.ADMIN_START)
+        case _:
+            await bot.send_message(message.from_user.id, 'Пожалуйста выбери одну из кнопок:',
+                                   reply_markup=try_again_keyboard)
+            await state.set_state(BotStates.TRY_AGAIN)
